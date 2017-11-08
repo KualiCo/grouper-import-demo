840617f8b88c08002600b8c3c1a4f3ea5d1f1b5c
Andre Kradolfer <narfdre@gmail.com>
2017-11-08 14:02:47 -0700

    adding command line articles

---
+++ groups.js
@@ -0,0 +1,127 @@
+const axios = require('axios')
+const Bluebird = require('bluebird')
+const { get, find } = require('lodash')
+
+const {
+  grouperUsername,
+  grouperPassword,
+  grouperBase,
+  kualiToken,
+  kualiBase
+} = require('config')
+
+const groupsPath = '/grouper-ws/servicesRest/v2_2_000/groups/'
+const subjectsPath = '/grouper-ws/servicesRest/v2_2_000/subjects'
+
+module.exports.run = async function () {
+  const response = await axios.post(
+    `${grouperBase}${groupsPath}`,
+    {
+      WsRestFindGroupsRequest: {
+        wsQueryFilter: {
+          stemName: 'groups',
+          queryFilterType: 'FIND_BY_STEM_NAME'
+        }
+      }
+    },
+    {
+      headers: {
+        'Content-Type': 'text/x-json',
+        Authorization: 'Basic YmFuZGVyc29uOnBhc3N3b3Jk'
+      }
+    }
+  )
+  const grouperResults = get(response, 'data.WsFindGroupsResults.groupResults')
+  const {
+    data: kualiGroups
+  } = await axios.get(`${kualiBase}/api/v1/groups/?limit=10000`, {
+    headers: {
+      Authorization: `Bearer ${kualiToken}`,
+      'Content-Type': 'application/json'
+    }
+  })
+  await Bluebird.map(grouperResults, async g => {
+    const newGroup = {
+      name: g.displayExtension,
+      fields: [
+        {
+          id: 'grouperId',
+          value: g.uuid
+        }
+      ]
+    }
+    const existingGroup = find(
+      kualiGroups,
+      kg => g.uuid === getField(kg, 'grouperId')
+    )
+    if (existingGroup) {
+      await updateGroup(existingGroup, newGroup)
+    } else {
+      await createGroup(newGroup)
+    }
+  })
+}
+
+module.exports.deleteAllGroups = async function () {
+  const {
+    data: kualiGroups
+  } = await axios.get(`${kualiBase}/api/v1/groups/?limit=10000`, {
+    headers: {
+      Authorization: `Bearer ${kualiToken}`,
+      'Content-Type': 'application/json'
+    }
+  })
+  console.log('kualiGroups.length', kualiGroups.length)
+  await Bluebird.map(
+    kualiGroups,
+    async kg => {
+      if (getField(kg, 'grouperId')) {
+        const status = await deleteGroup(kg)
+        console.log('status', status)
+      }
+    },
+    { concurrency: 100 }
+  )
+}
+
+function getField (group, fieldId) {
+  let result
+  if (group.fields) {
+    const field = find(group.fields, f => f.id === fieldId)
+    if (field) {
+      result = field.value
+    }
+  }
+
+  return result
+}
+
+async function createGroup (newGroup) {
+  await axios.post(`${kualiBase}/api/v1/groups/`, newGroup, {
+    headers: {
+      Authorization: `Bearer ${kualiToken}`,
+      'Content-Type': 'application/json'
+    }
+  })
+}
+
+async function deleteGroup (group) {
+  const resp = await axios.delete(`${kualiBase}/api/v1/groups/${group.id}/`, {
+    headers: {
+      Authorization: `Bearer ${kualiToken}`,
+      'Content-Type': 'application/json'
+    }
+  })
+  // console.log('resp', resp)
+  return resp.status
+}
+
+async function updateGroup (existingGroup, newGroup) {
+  const updated = Object.assign({}, existingGroup, newGroup)
+  await axios.put(`${kualiBase}/api/v1/groups/${updated.id}`, updated, {
+    headers: {
+      Authorization: `Bearer ${kualiToken}`,
+      'Content-Type': 'application/json'
+    }
+  })
+}
---
+++ users.js
@@ -0,0 +1,74 @@
+var ldap = require('ldapjs')
+var axios = require('axios')
+const Bluebird = require('bluebird')
+
+const { kualiToken, kualiBase, ldapUrl } = require('config')
+
+const SCHOOL_ID = '3j33134HH'
+
+var client = ldap.createClient({
+  url: ldapUrl
+})
+
+const headers = {
+  Authorization: `Bearer ${kualiToken}`,
+  'Content-Type': 'application/json'
+}
+
+module.exports.loadUsers = function () {
+  client.search(
+    'ou=people,dc=example,dc=edu',
+    { filter: '(objectclass=person)', scope: 'sub' },
+    function (err, res) {
+      console.log('search ERROR:', err)
+
+      let users = []
+
+      res.on('searchEntry', function (entry) {
+        const user = {
+          name: entry.object.cn,
+          username: entry.object.uid,
+          email: entry.object.mail,
+          role: 'user',
+          firstName: entry.object.givenName,
+          lastName: entry.object.sn,
+          password: 'password',
+          passwordConfirmation: 'password',
+          schoolId: SCHOOL_ID,
+          approved: true,
+          active: true
+        }
+
+        users.push(user)
+      })
+
+      res.on('error', function (err) {
+        console.error('error: ' + err.message)
+      })
+
+      res.on('end', async function (result) {
+        await Bluebird.map(users, async user => {
+          const response = await axios.post(`${kualiBase}/api/v1/users`, user, {
+            headers
+          })
+          console.log('SAVE RESPONSE STATUS ', response.status)
+        }, { concurrency: 100 })
+        console.log('Search ended with status: ' + result.status)
+      })
+    }
+  )
+}
+
+module.exports.deleteUsers = async function () {
+  console.log('FETCHING ALL USERS')
+  const response = await axios.get(`${kualiBase}/api/v1/users?schoolId=${SCHOOL_ID}`, { headers })
+  const users = response.data
+
+  await Bluebird.map(users, async user => {
+    console.log('GONNA DELETE ', user.username)
+    const res = await axios.delete(`${kualiBase}/api/v1/users/${user.id}`, { headers })
+    console.log('respose from delete', res.status)
+  }, { concurrency: 100 })
+
+  return 'done'
+}
