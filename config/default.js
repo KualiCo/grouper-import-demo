8f80dc47fc5607bad36a7c379fb1dd34038d14b1
Michael Graybeal <michael@thegraybeals.com>
2017-11-08 09:26:07 -0700

    working for importing groups into kuali

---
+++ config/default.js
@@ -0,0 +1,7 @@
+module.exports = {
+  grouperUsername: 'banderson',
+  grouperPassword: 'password',
+  grouperBase: 'http://localhost:8080',
+  kualiToken: 'a-token',
+  kualiBase: 'https://graybeal-tst.kuali.co'
+}
