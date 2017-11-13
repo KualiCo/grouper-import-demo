9a2ff9fcc4a2b53d101012e7feb24ad644607070
Michael Graybeal <michael@thegraybeals.com>
2017-11-13 16:24:58 -0700

    added an ldapurl to default

--- config/default.js
+++ config/default.js
@@ -3,5 +3,6 @@ module.exports = {
   grouperPassword: 'password',
   grouperBase: 'http://localhost:8080',
   kualiToken: 'a-token',
-  kualiBase: 'https://graybeal-tst.kuali.co'
+  kualiBase: 'https://graybeal-tst.kuali.co',
+  ldapUrl: 'ldap://localhost'
 }
